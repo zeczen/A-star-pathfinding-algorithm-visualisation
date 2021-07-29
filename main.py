261bb8035aa79464aad2918173bececb8f35e103
eyal seckbach <54035934+zeczen@users.noreply.github.com>
2021-07-29 12:03:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 from Square import Square
-import time
 
 TICK_SECOND = 50
 start = stop = None
@@ -26,6 +25,7 @@ def update_sprites():
 
 def update_single_sprite(sq):
     # more efficient then 'update_sprites'
+    # because we are updating the square that has changed instead of updating them all
     sq.draw(display_surface)
     pygame.display.flip()
 
@@ -34,7 +34,7 @@ def find_index(x, y):
     return (x // (Square.SQUARE_WIDTH + 1)), (y // (Square.SQUARE_WIDTH + 1))
 
 
-def start_game():
+def initial_game():
     global main_list, start, stop
     finish = False
     drag = False
@@ -42,37 +42,39 @@ def start_game():
     while not finish:
 
         for event in pygame.event.get():
+            sq = None
 
             if event.type == pygame.QUIT:
-                pygame.quit()
+                raise Exception()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 button_pressed = event.button
-            if drag and button_pressed is 1:
+            if drag and button_pressed == 1:
                 x_pos, y_pos = pygame.mouse.get_pos()
                 x_index, y_index = find_index(x_pos, y_pos)
                 sq = main_list[y_index][x_index]
                 sq.set_sit("Block")
-            elif drag and button_pressed is 2:
+            elif drag and button_pressed == 2:
                 x_pos, y_pos = pygame.mouse.get_pos()
                 x_index, y_index = find_index(x_pos, y_pos)
                 sq = main_list[y_index][x_index]
-                if sq.sit is "Edge":
+                if sq.sit == "Edge":
                     start = None
                     # its have to be 'start', if it was 'stop' the program was run already
                 sq.set_sit("Null")
-            elif event.type == pygame.MOUSEBUTTONDOWN and button_pressed is 3:
+            elif event.type == pygame.MOUSEBUTTONDOWN and button_pressed == 3:
                 x_pos, y_pos = pygame.mouse.get_pos()
                 x_index, y_index = find_index(x_pos, y_pos)
                 sq = main_list[y_index][x_index]
                 sq.set_sit("Edge")
-                if start is None or start.sit is not "Edge":
+                if start is None or not start.sit == "Edge":
                     start = sq
                 else:
                     stop = sq
                     if not stop == start:
                         finish = True
             drag = event.type == pygame.MOUSEBUTTONDOWN or drag and not event.type == pygame.MOUSEBUTTONUP
-        update_sprites()
+
+            if sq is not None:  update_single_sprite(sq)
 
 
 def update_cost_squares(pos):
@@ -108,7 +110,7 @@ def update_cost_squares(pos):
 
 
 def lowest_score_point():
-    if len(border_sq) is 0:
+    if len(border_sq) == 0:
         return None
     sq = min(border_sq)
     border_sq.remove(sq)
@@ -116,6 +118,31 @@ def lowest_score_point():
     update_single_sprite(sq)
 
     return sq  # the square with the lowest rank
+
+
+def stats(length_path, sum_path, distance):
+    efficiency = (start.rank * distance) / sum_path
+    # the lowest possible ranking divides the actual ranking
+
+    length_score = distance / length_path
+    # the shortest route (without block) divided by the current path
+
+    efficiency *= 100
+    length_score *= 100
+    # in presents
+
+    efficiency = int(1000 * efficiency) / 1000
+    length_score = int(1000 * length_score) / 1000
+    # show 3 digit after the point
+
+    print(
+        f'''
+        finish successfully:
+            path efficiency score = {efficiency} %
+            path length score = {length_score} %
+            length path = {length_path}
+            '''
+    )
 
 
 def on_finish():
@@ -136,84 +163,93 @@ def on_finish():
 
     main_list = list(
         map(lambda lst: [sq if sq.sit in ['Edge', 'Block', 'Path'] else sq.set_sit('Null') for sq in lst], main_list))
+    start.set_sit('Edge')
+    stop.set_sit('Edge')
+    update_sprites()
+    # to show the path on the map
+
+    distance = abs(x_start - x_stop) + abs(y_start - y_stop)
+    # this is the distance in a straight trajectory, without diagonals
+
+    start.addRank(start, stop, border_sq)
+
+    stats(length_path, sum_path, distance)
+
+    return
+
+
+def on_stuck():
+    print("there is no way.")
+    return
+
+
+def the_game():
+    global start, stop
+
+    pos = start
+    run = True
+    finish = False
+
+    while not finish:
+        if run:
+            update_cost_squares(pos)
+            pos = lowest_score_point()
+            if pos == start: pos.set_sit('Edge')
+            if pos is None or pos == stop:
+                finish = True
+
+            clock.tick(TICK_SECOND)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                raise Exception()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                run = not run
+
+    if pos is None:
+        return on_stuck()
+    if pos == stop:
+        return on_finish()
+        # if we finish successfully
+
+
+def main():
+    global start, stop, main_list, border_sq
 
     update_sprites()
-
-    efficient_length = abs(x_start - x_stop) + abs(y_start - y_stop)
-    efficiency = (start.rank * efficient_length) / sum_path
-    efficiency *= 100
-    # in presents
-
-    print(f'''
-finish successfully:
-    path efficiency score = {int(1000 * efficiency) / 1000} %
-    path length score = {int(1000 * 100 * (efficient_length / length_path)) / 1000} %
-    length path = {length_path}
-    ''')
-
-    start_again()
-
-
-def start_again():
-    global start, stop, main_list, border_sq
-    border_sq = []
-    start = stop = None
-    main_list = [[Square(x, y) for x in range(1, Square.BOARD_WIDTH, Square.SQUARE_WIDTH + 1)]
-                 for y in range(1, Square.BOARD_HEIGHT, Square.SQUARE_HEIGHT + 1)]
-    print("click for new map")
+    initial_game()
+    the_game()
+    # the first game
 
     while True:
         # wait to the user interaction
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
+                raise Exception()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                return main()
+                border_sq.clear()
+                start = stop = None
+                main_list = [[Square(x, y) for x in range(1, Square.BOARD_WIDTH, Square.SQUARE_WIDTH + 1)]
+                             for y in range(1, Square.BOARD_HEIGHT, Square.SQUARE_HEIGHT + 1)]
+                # initialize properties for a new game
 
-
-def on_stuck():
-    global start, stop, main_list
-
-    print("there is no way.")
-    start_again()
-
-
-def main():
-    global start, stop
-    start_game()
-
-    pos = start
-
-    finish = False
-    while not finish:
-        update_cost_squares(pos)
-        pos = lowest_score_point()
-        if pos is None or pos == stop:
-            finish = True
-
-        #        update_sprites()
-        clock.tick(TICK_SECOND)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                finish = True
-
-    if pos is None:
-        on_stuck()
-    if pos == stop:
-        on_finish()
+                update_sprites()
+                initial_game()
+                the_game()
 
 
 if __name__ == "__main__":
-    print('''
+    print(
+        '''
         ========================================
-         - right button for start and stop
-         - left button for a wall
-         - middle button to erase 
+         - Right button for start and end
+         - Left button to create a block / start new map / stop and continue running
+         - Middle button to erase 
         ========================================
-    ''')
-    main()
-
-
-pygame.quit()
+    '''
+    )
+    try:
+        main()
+    except Exception as e:
+        pygame.quit()
